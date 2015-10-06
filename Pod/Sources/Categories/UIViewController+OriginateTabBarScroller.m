7ede3b075863d206f776684b463a41fb3b13802c
Will Tachau <wtachau@gmail.com>
2015-10-06 13:49:35 -0700

    checks for initial content offset

--- Pod/Sources/Categories/UIViewController+OriginateTabBarScroller.m
+++ Pod/Sources/Categories/UIViewController+OriginateTabBarScroller.m
@@ -11,6 +11,7 @@
 
 @implementation UIViewController (OriginateTabBarScroller)
 
+// The previous scroll offset
 - (void)setOri_PreviousScrollViewOffset:(CGFloat)previousScrollViewOffset
 {
     objc_setAssociatedObject(self, @selector(ori_previousScrollViewOffset), @(previousScrollViewOffset), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
@@ -19,6 +20,17 @@
 - (CGFloat)ori_previousScrollViewOffset
 {
     return [objc_getAssociatedObject(self, @selector(ori_previousScrollViewOffset)) floatValue];
+}
+
+// The initial content offset
+- (void)setOri_InitialContentOffset:(CGFloat)contentOffset
+{
+    objc_setAssociatedObject(self, @selector(ori_initialContentOffset), @(contentOffset), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (CGFloat)ori_initialContentOffset
+{
+    return [objc_getAssociatedObject(self, @selector(ori_initialContentOffset)) floatValue];
 }
 
 - (void)slideTabBar:(UITabBar *)tabBar inResponseToScrollInScrollView:(UIScrollView *)scrollView
@@ -34,6 +46,11 @@
     CGFloat scrollOffset = scrollView.contentOffset.y;
     CGFloat deltaScrollOffset = (scrollOffset - self.ori_previousScrollViewOffset);
     [self setOri_PreviousScrollViewOffset:scrollOffset];
+    
+    // Calculate the initial content offset
+    if (!self.ori_initialContentOffset) {
+        [self setOri_InitialContentOffset:scrollOffset];
+    }
 
     // Calculate what the offset would be, assuming the tab bar wouldn't go off screen
     CGFloat potentialTabBarOffset = (tabBarFrame.origin.y - originalTabBarOrigin) + deltaScrollOffset;
@@ -47,7 +64,7 @@
     // Only change tab bar if the user hasn't scrolled off above or below the scroll view
     CGFloat scrollHeight = scrollView.frame.size.height;
     BOOL isAtBottomOfScrollView = (scrollOffset + scrollHeight) >= scrollView.contentSize.height;
-    if (scrollOffset > 0 && !isAtBottomOfScrollView) {
+    if (scrollOffset > self.ori_initialContentOffset && !isAtBottomOfScrollView) {
         tabBarFrame.origin.y = originalTabBarOrigin + optimalTabBarOffset;
         tabBar.frame = tabBarFrame;
     }
